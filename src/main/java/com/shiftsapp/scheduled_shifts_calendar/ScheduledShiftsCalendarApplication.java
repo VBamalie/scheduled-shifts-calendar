bb201cdf0b0f6f54d621d1c9f5a1a12a64b8ccca
VBamalie <vbamalie@gmail.com>
2024-06-06 13:34:12 -0400

    first commit

---
+++ src/main/java/com/shiftsapp/scheduled_shifts_calendar/ScheduledShiftsCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.shiftsapp.scheduled_shifts_calendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduledShiftsCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduledShiftsCalendarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shiftsapp/scheduled_shifts_calendar/ScheduledShiftsCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shiftsapp.scheduled_shifts_calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduledShiftsCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
